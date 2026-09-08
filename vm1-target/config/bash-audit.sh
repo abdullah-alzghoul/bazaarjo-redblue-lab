5e0357f02c3ef4eda7e6efea5a171f6b3fe8ed1d
abdullah-alzghoul <abdullahzghoul777@gmail.com>
2026-09-08 20:37:11 +0300

    Enhance bash-audit.sh for SIEM and Zsh support
    Updated bash command auditing to support SIEM forwarding and added support for Zsh shell.

--- vm1-target/config/bash-audit.sh
+++ vm1-target/config/bash-audit.sh
@@ -1,2 +1,9 @@
-# /etc/profile.d/audit.sh - Log all bash commands to syslog
-export PROMPT_COMMAND='history -a; logger -t bash_audit "$(whoami) [$$]: $(history 1 | sed "s/^[ ]*[0-9]*[ ]*//")"'
+# Real-time bash command auditing for SIEM forwarding
+export PROMPT_COMMAND='logger -p local1.info -t bash_audit "$(whoami)@$(hostname): $(history 1 | sed "s/^[ ]*[0-9]*[ ]*//")"'
+
+# Support for Zsh Shell (Kali Linux default)
+if [ -n "$ZSH_VERSION" ]; then
+  precmd() {
+    logger -p local1.info -t bash_audit "$(whoami)@$(hostname): $(fc -ln -1)"
+  }
+fi
