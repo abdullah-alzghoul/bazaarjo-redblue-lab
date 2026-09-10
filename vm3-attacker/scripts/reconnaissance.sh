8a1bb21da0ce0a3198a9d961af1509fedf1261bf
abdullah-alzghoul <abdullahzghoul777@gmail.com>
2026-09-10 13:29:40 +0300

    Update reconnaissance.sh

--- vm3-attacker/scripts/reconnaissance.sh
+++ vm3-attacker/scripts/reconnaissance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Initial Reconnaissance Script against VM1 Target
-TARGET_IP="192.168.56.101"
+source ../config/target.conf
 
 echo "[*] Running Nmap Port Scan on $TARGET_IP..."
 nmap -sV -sC -p- $TARGET_IP -oN nmap_initial.txt
